c45dbde0904f9148096eda1b04cd92cfc66c845c
sandhuja <35586551+sandhuja@users.noreply.github.com>
2020-07-08 12:29:47 -0500

    Update entrypoint.sh

--- spark-hadoop-docker/entrypoint.sh
+++ spark-hadoop-docker/entrypoint.sh
@@ -75,6 +75,14 @@ for f in $SPARK_HOME/jars/*.jar; do
   fi
 done
 
+for f in $SPARK_HOME/jars/minio/*.jar; do
+  if [ "$SPARK_CLASSPATH" ]; then
+    export SPARK_CLASSPATH=$SPARK_CLASSPATH:$f
+  else
+    export SPARK_CLASSPATH=$f
+  fi
+done
+
 
 export SPARK_DIST_CLASSPATH=$(hadoop classpath)
 export HADOOP_CLASSPATH=$SPARK_DIST_CLASSPATH
